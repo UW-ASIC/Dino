985e3d469ed0f7db8d359b61e89aef7b6ed954b4
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-01-26 20:49:19 -0500

    Fix the module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none 
  
-module tt_uwasic_dinogame (
+module tt_um_uwasic_dinogame (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
