37cd9d59b7aa42ab383db4a20b740f134d1310f7
Penguronik <ronikant12345@gmail.com>
2025-02-23 18:26:00 -0500

    top changes

--- src/player_controller.v
+++ src/player_controller.v
@@ -7,11 +7,11 @@ module player_controller (
   input button_up,
   input button_down,
   input crash,
-  output [5:0] player_position,
+  output [5:0] player_position /*verilator public*/,
   output game_start_pulse,
   output game_over_pulse,
   output jump_pulse,
-  output reg [2:0] game_state
+  output reg [2:0] game_state /*verilator public*/
 );
 
   localparam RESTART   = 3'b000;
