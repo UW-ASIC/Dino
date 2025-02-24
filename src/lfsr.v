41f6a87bfc9f5f3d21d4e3c73ca8ff107ee51db7
leozqi <leo@leozqi.com>
2025-02-23 19:05:05 -0500

    Add named blocks for LFSR case

--- src/lfsr.v
+++ src/lfsr.v
@@ -15,20 +15,20 @@ module lfsr
   // http://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
   generate
     case (NUM_BITS)
-      3: assign r_xnor = r_lfsr[3] ^~ r_lfsr[2];
-      4: assign r_xnor = r_lfsr[4] ^~ r_lfsr[3];
-      5: assign r_xnor = r_lfsr[5] ^~ r_lfsr[3];
-      6: assign r_xnor = r_lfsr[6] ^~ r_lfsr[5];
-      7: assign r_xnor = r_lfsr[7] ^~ r_lfsr[6];
-      8: assign r_xnor = r_lfsr[8] ^~ r_lfsr[6] ^~ r_lfsr[5] ^~ r_lfsr[4];
-      9: assign r_xnor = r_lfsr[9] ^~ r_lfsr[5];
-      10: assign r_xnor = r_lfsr[10] ^~ r_lfsr[7];
-      11: assign r_xnor = r_lfsr[11] ^~ r_lfsr[9];
-      12: assign r_xnor = r_lfsr[12] ^~ r_lfsr[6] ^~ r_lfsr[4] ^~ r_lfsr[1];
-      13: assign r_xnor = r_lfsr[13] ^~ r_lfsr[4] ^~ r_lfsr[3] ^~ r_lfsr[1];
-      14: assign r_xnor = r_lfsr[14] ^~ r_lfsr[5] ^~ r_lfsr[3] ^~ r_lfsr[1];
-      15: assign r_xnor = r_lfsr[15] ^~ r_lfsr[14];
-      16: assign r_xnor = r_lfsr[16] ^~ r_lfsr[15] ^~ r_lfsr[13] ^~ r_lfsr[4];
+      3: begin : gen_poly assign r_xnor = r_lfsr[3] ^~ r_lfsr[2]; end
+      4: begin : gen_poly assign r_xnor = r_lfsr[4] ^~ r_lfsr[3]; end
+      5: begin : gen_poly assign r_xnor = r_lfsr[5] ^~ r_lfsr[3]; end
+      6: begin : gen_poly assign r_xnor = r_lfsr[6] ^~ r_lfsr[5]; end
+      7: begin : gen_poly assign r_xnor = r_lfsr[7] ^~ r_lfsr[6]; end
+      8: begin : gen_poly assign r_xnor = r_lfsr[8] ^~ r_lfsr[6] ^~ r_lfsr[5] ^~ r_lfsr[4]; end
+      9: begin : gen_poly assign r_xnor = r_lfsr[9] ^~ r_lfsr[5]; end
+      10: begin : gen_poly assign r_xnor = r_lfsr[10] ^~ r_lfsr[7]; end
+      11: begin : gen_poly assign r_xnor = r_lfsr[11] ^~ r_lfsr[9]; end
+      12: begin : gen_poly assign r_xnor = r_lfsr[12] ^~ r_lfsr[6] ^~ r_lfsr[4] ^~ r_lfsr[1]; end
+      13: begin : gen_poly assign r_xnor = r_lfsr[13] ^~ r_lfsr[4] ^~ r_lfsr[3] ^~ r_lfsr[1]; end
+      14: begin : gen_poly assign r_xnor = r_lfsr[14] ^~ r_lfsr[5] ^~ r_lfsr[3] ^~ r_lfsr[1]; end
+      15: begin : gen_poly assign r_xnor = r_lfsr[15] ^~ r_lfsr[14]; end
+      16: begin : gen_poly assign r_xnor = r_lfsr[16] ^~ r_lfsr[15] ^~ r_lfsr[13] ^~ r_lfsr[4]; end
     endcase // case (NUM_BITS)
   endgenerate
 
