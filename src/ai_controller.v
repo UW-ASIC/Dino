61b3d3ba7db99fa78141609c3391bc7411e299d2
Ernest Tang <tang.ernest@yahoo.ca>
2025-03-02 14:53:14 -0800

    Small improvements to ai_controller

--- src/ai_controller.v
+++ src/ai_controller.v
@@ -8,7 +8,7 @@ module ai_controller
   input [9:CONV] obstacle1_pos,
   input [9:CONV] obstacle2_pos,
   input crash,
-  output button_up,
+  output reg button_up,
   output reg crash_out
 );
 
@@ -21,6 +21,7 @@ always @(posedge clk) begin
   if (!rst_n) begin
     button_up <= 1'b0;
     crash_out <= 1'b0;
+    restart_counter <= 'b0;
   end else begin
     if (crash_out) begin
       restart_counter <= restart_counter + 1;
@@ -32,9 +33,7 @@ always @(posedge clk) begin
     end else if (crash) begin
       crash_out <= 1'b1;
     end else begin
-      if (obstacle1_pos <= obstacle_threshold && obstacle1_pos > PLAYER_OFFSET) begin
-        button_up <= 1'b1;
-      end else if (obstacle2_pos <= obstacle_threshold && obstacle2_pos > PLAYER_OFFSET) begin
+      if ((obstacle1_pos <= obstacle_threshold && obstacle1_pos > PLAYER_OFFSET) || (obstacle2_pos <= obstacle_threshold && obstacle2_pos > PLAYER_OFFSET)) begin
         button_up <= 1'b1;
       end else begin
         button_up <= 1'b0;
