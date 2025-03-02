12748c5f267910eaf5456e904501740689e94897
Penguronik <ronikant12345@gmail.com>
2025-03-02 16:03:59 -0500

    remove vpos[5] for debounce

--- src/graphics_top.v
+++ src/graphics_top.v
@@ -18,14 +18,12 @@ module graphics_top #(parameter CONV = 0)(
   output reg o_game_tick_60hz,
   output reg o_game_tick_20hz,
   output reg o_game_tick_20hz_r,
-  output reg o_vpos_5_r,
   output reg o_collision
 );
     // ============== HVSYNC =============
     // TODO can change hpos to increment by 2 to reduce bits
     reg [9:0] hpos;
     reg [9:0] vpos;
-    reg vpos_5_r;
     reg display_on;
     // TODO can remove this pipeline stage if we don't need it
     reg hsync;
@@ -57,7 +55,6 @@ module graphics_top #(parameter CONV = 0)(
         end else begin
             // vsync_r <= vsync;
             // hsync_r <= hsync;
-            vpos_5_r <= vpos[5];
             // display_on_r <= display_on;
         end
     end
@@ -118,7 +115,6 @@ module graphics_top #(parameter CONV = 0)(
         o_game_tick_60hz = (vpos == 0) && (hpos == 0);
         o_game_tick_20hz = frame_counter == 1 && o_game_tick_60hz;
         o_game_tick_20hz_r = game_tick_r;
-        o_vpos_5_r = (vpos[5] == 1) && (vpos_5_r == 0);
         o_collision = i_color_obstacle && i_color_player;
     end
 
