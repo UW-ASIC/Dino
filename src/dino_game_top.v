653226b8a15d4a849ecf64dd8abb5f6db0f0f0cc
Roni Kant <35043400+Penguronik@users.noreply.github.com>
2025-03-10 00:50:11 -0400

    Update dino_game_top.v

--- src/dino_game_top.v
+++ src/dino_game_top.v
@@ -258,6 +258,6 @@ module tt_um_uwasic_dinogame #(parameter CONV = 2) (
     assign uio_oe  = 8'b10000000;
 
     // List all unused inputs to prevent warnings
- wire _unused = &{ena, ui_in[7], ui_in[3:0], uio_in, gamepad_start, gamepad_b, gamepad_y, gamepad_select, gamepad_left, gamepad_right, gamepad_a, gamepad_x, gamepad_l, gamepad_r, 1'b0};
+ wire _unused = &{ena, ui_in[7], ui_in[3:0], uio_in, gamepad_start, gamepad_b, gamepad_y, gamepad_select, gamepad_left, gamepad_right, gamepad_a, gamepad_x, gamepad_l, gamepad_r, gamepad_is_present, 1'b0};
 
 endmodule
