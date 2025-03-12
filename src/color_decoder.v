ef97a45267a997e320af0a20e953f7062b0b0101
Damir Gazizullin <dgazizul@uwaterloo.ca>
2025-03-12 00:40:23 -0400

    Refactor color decoder logic and update signal connections in game modules

--- src/color_decoder.v
+++ src/color_decoder.v
@@ -10,24 +10,24 @@ module color_decoder_2_6 (
   always @(*) begin
     case (layer)
       2'b00: begin
-        R = 2'b00;
-        G = 2'b11;
-        B = 2'b11;
+        R = 2'b01;
+        G = 2'b10;
+        B = 2'b10;
       end
       2'b01: begin
-        R = 2'b11;
+        R = 2'b00;
         G = 2'b11;
         B = 2'b00;
       end
       2'b10: begin
-        R = 2'b00;
-        G = 2'b11;
+        R = 2'b10;
+        G = 2'b10;
         B = 2'b00;
       end
       2'b11: begin
-        R = 2'b10;
-        G = 2'b10;
-        B = 2'b10;
+        R = 2'b00;
+        G = 2'b00;
+        B = 2'b00;
       end
       default: begin
         R = 2'b00;
@@ -36,15 +36,14 @@ module color_decoder_2_6 (
       end
     endcase
     if (~rgb_scheme) begin
-      // Cyan for all layers when rgb_scheme is high
-      R = 2'b11;
-      G = 2'b11;
-      B = 2'b11;
-    end
-    if (~is_colored) begin
       R = 2'b00;
       G = 2'b00;
       B = 2'b00;
+    end
+    if (~is_colored) begin
+      R = 2'b11;
+      G = 2'b11;
+      B = 2'b11;
     end
     if (invert) begin
       R = ~R;
